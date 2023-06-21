406da4895ef899134b8d0e8fb031b88f147aaec3
asc33nzio <amadeo.f.marli@gmail.com>
2023-06-22 04:12:41 +0700

    necessary updates to handle our lovely API (in which i totally take no part in designing)

--- src/pages/verify.jsx
+++ src/pages/verify.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { Box, Text, Flex, Alert, AlertIcon, CloseButton, Spinner, Stack, Button } from '@chakra-ui/react';
 
