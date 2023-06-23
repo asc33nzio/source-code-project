6a4f8bf11d8ef77fff442653068dcb4b6496c1f4
asc33nzio <amadeo.f.marli@gmail.com>
2023-06-23 21:29:13 +0700

    adjustments due to last minute API changes(for the API provider, a vehement fuck you for not doing it right the first time), major components and UI changes, added God Mode

--- src/pages/verify_auto.jsx
+++ src/pages/verify_auto.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
-import { Box, Text, Flex, Alert, AlertIcon, CloseButton, Spinner, Stack, Button } from '@chakra-ui/react';
+import { Box, Flex, Alert, AlertIcon, CloseButton, Spinner, Button } from '@chakra-ui/react';
 
 export const VerifyAuto = () => {
     const { token } = useParams();
